0a90c3f034fa93d1bc2c19de756be2b0268b12f4
Alexander Bastidas <xanderjorge@hotmail.com>
2024-10-29 16:09:28 -0500

    run server

--- App/run.py
+++ App/run.py
@@ -10,5 +10,13 @@ def pagina_principal():
 def login_user():
     return render_template('login.html')
 
+@app.route('/resgistro', methods = ['POST'])
+def user_registrer():
+    return render_template('registro.html')
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('error_404.html')
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
