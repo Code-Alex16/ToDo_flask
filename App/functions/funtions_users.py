25bfe37998c6fd243ca980fde1be15d376b45c78
Alexander Bastidas <xanderjorge@hotmail.com>
2024-10-31 20:40:09 -0500

    functions for app

---
+++ App/functions/funtions_users.py
@@ -0,0 +1,13 @@
+from Database.config import Database
+
+def validated_users(user = None, password = None):
+    #TODO -> validar si el usuario se encuentra en la base de datos
+    pass
+
+def registrer_user():
+    #TODO -> registrar un usuario en la base de datos
+    pass
+
+def encripted_password():
+    #TODO -> encriptar la contraseña del usuario
+    pass
