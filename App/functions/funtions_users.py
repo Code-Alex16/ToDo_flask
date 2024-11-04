cb3ea3a0d2121c87afe9a31cab23623bfa3db6d6
Alexander Bastidas <xanderjorge@hotmail.com>
2024-11-04 14:11:33 -0500

    Update

--- App/functions/funtions_users.py
+++ App/functions/funtions_users.py
@@ -1,13 +1,79 @@
-from Database.config import Database
+from App.Database.db_config import Database_connexion
+from werkzeug.security import generate_password_hash, check_password_hash
 
-def validated_users(user = None, password = None):
-    #TODO -> validar si el usuario se encuentra en la base de datos
-    pass
+import re
 
-def registrer_user():
-    #TODO -> registrar un usuario en la base de datos
-    pass
+def validated_users(user_email : str = None, password = None):
+    #detectar si ingresa con username o email
+    is_email = False
 
-def encripted_password():
-    #TODO -> encriptar la contraseña del usuario
-    pass
+    if re.search('@', user_email):
+        is_email = True
+
+    # Seleccionar el tipo de busqueda (si usa username o email)
+    query = 'SELECT password FROM tbl_users WHERE user_name = %s'
+
+    if is_email:
+        query = 'SELECT password FROM tbl_users WHERE email = %s'
+    
+    values = (user_email,)
+
+    #Buscar en la base de datos y comparar
+    try:
+        con = Database_connexion.GetConnexion()
+        cursor = con.cursor()
+        password_hash = cursor.execute(query,values)
+
+    except Exception as e:
+        print(f'Error : {e}')
+
+    finally:
+        # cerrar conexiones
+        cursor.close()
+        Database_connexion.CloseConexion(connection=con)
+
+        # comparar y retornar
+        if password_hash and check_password_hash(password_hash,password):
+            return True
+        else:
+            return False
+
+def registrer_user(user_name, email, password):
+    # validar datos sean correctos
+    is_email = False
+    is_username = False
+    password_hash = generate_password_hash(method='scrypt', salt_length=16, password=password)
+
+    if re.search('@', email):
+        is_email = True
+
+    if (len(user_name) < 12):
+        is_username = True
+
+    if (is_email and is_username):
+        try:
+            con = Database_connexion.GetConnexion()
+            cursor = con.cursor()
+            sql = 'INSERT INTO tbl_users (user_name, email, password) VALUES (%s,%s,%s)'
+            values = (user_name, email, password_hash)
+            
+            cursor.execute(sql, values)
+            con.commit()
+            
+        except Exception as ex:
+            print(f'Error al ingresar el usuario: {ex}')
+        
+        finally:
+            cursor.close()
+            Database_connexion.CloseConexion(connection=con)
+
+
+'''
+clave = 'prueba'
+
+clave_hash = generate_password_hash(method='scrypt', salt_length=16, password=clave)
+print(clave_hash)
+
+if (check_password_hash(pwhash=clave_hash, password=clave)):
+    print(f'si son iguales {clave_hash} : {clave}')
+'''
--- App/run.py
+++ App/run.py
@@ -1,7 +1,15 @@
+import os
+from dotenv import load_dotenv
+from functions.funtions_users import validated_users, registrer_user
 from flask import Flask, render_template, request, redirect, url_for
-from functions.funtions_users import *
-app = Flask(__name__)
 
+load_dotenv()   #cargar variables de entorno
+
+#configurar app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+## USUARIOS ##
 @app.route('/')
 def pagina_principal():
     return render_template('pagina_principal.html')
@@ -13,16 +21,30 @@ def login_user():
         password = request.form.get('password')
 
         if not(validated_users(user_email, password)):
-            return redirect(url_for('login_user', msg='Usuario no existente'))
+            return redirect(url_for('login_user', msg='Contraseña no valida'))
         
         return redirect(url_for('pagina_usuario'))
         
     return render_template('login.html')
 
-@app.route('/resgistro', methods = ['POST'])
+@app.route('/resgistro', methods = ['POST','GET'])
 def user_registrer():
+
+    if request.method == 'POST':
+        user_name = request.form.get('user_name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if not(registrer_user(user_name,email,password)):
+            return redirect(url_for('user_registrer', msg='datos invalidos'))
+        
+        return redirect(url_for('login_user', msg = 'Ahora puede Iniciar sesion'))
+
     return render_template('registro.html')
 
+## Task ##
+
+## ERRORES ##
 @app.errorhandler(404)
 def page_not_found():
     return render_template('error_404.html')
